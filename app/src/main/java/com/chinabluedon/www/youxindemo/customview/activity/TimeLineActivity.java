575569caa707097c6f84a1095d9d9249ed0a8aa7
huteng <499019771@qq.com>
2017-09-21 15:31:28 +0800

    提交时间轴自定义控件代码

--- app/src/main/java/com/chinabluedon/www/youxindemo/customview/activity/TimeLineActivity.java
+++ app/src/main/java/com/chinabluedon/www/youxindemo/customview/activity/TimeLineActivity.java
@@ -3,9 +3,12 @@ package com.chinabluedon.www.youxindemo.customview.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
 
 import com.chinabluedon.www.youxindemo.R;
+
+import java.math.BigDecimal;
 
 /**
  * @author ht
@@ -21,7 +24,8 @@ public class TimeLineActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
         mView = findViewById(R.id.timeLineView);
-
+        double round = round(2.444446, 4);
+        Log.e("TimeLineActivity",round+"");
     }
 
     @Override
@@ -35,6 +39,16 @@ public class TimeLineActivity extends Activity {
     protected void onResume () {
         super.onResume();
 
+    }
+
+    public static double round(double v, int scale) {
+        if (scale < 0) {
+            throw new IllegalArgumentException("The scale must be a positive integer or zero");
+        }
+
+        BigDecimal b = new BigDecimal(Double.toString(v));
+        BigDecimal one = new BigDecimal("1");
+        return b.divide(one, scale, BigDecimal.ROUND_HALF_UP).doubleValue();
     }
 
 }
