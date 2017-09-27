df3a2685247c5283d50c9023304ff8b208685c51
huteng <499019771@qq.com>
2017-09-27 17:53:34 +0800

    添加高效加载bitmap工具方法

---
+++ app/src/main/java/com/chinabluedon/www/youxindemo/utils/ImageViewUtils.java
@@ -0,0 +1,43 @@
+package com.chinabluedon.www.youxindemo.utils;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+/**
+ * @author ht
+ * @time 2017/9/27  17:40
+ * @desc ${TODD}
+ */
+public class ImageViewUtils {
+
+    public static Bitmap decodeSampleBitmapFromResource (Resources res, int resIs, int reqWidth, int reqHeight) {
+
+        //获取bitmap资源
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resIs, options);
+
+        //计算采集率的大小
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        options.inJustDecodeBounds = false;
+
+        return BitmapFactory.decodeResource(res, resIs, options);
+
+    }
+
+    private static int calculateInSampleSize (BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        int width = options.outWidth;
+        int height = options.outHeight;
+        int inSampleSize = 1;
+        if (width > reqWidth || height > reqHeight) {
+            int halfWidth=width/2;
+            int halfHeight=height/2;
+            while ((halfWidth/inSampleSize)>reqWidth&&(halfHeight/inSampleSize)>reqHeight){
+                inSampleSize=inSampleSize*2;
+            }
+        }
+        return inSampleSize;
+    }
+}
