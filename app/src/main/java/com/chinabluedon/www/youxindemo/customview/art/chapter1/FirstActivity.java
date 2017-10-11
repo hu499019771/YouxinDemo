ffb777b72418378490281f1597e2a294891500c0
hu499019771 <499019771@qq.com>
2017-10-11 22:18:32 +0800

    提交Android任务栈代码

---
+++ app/src/main/java/com/chinabluedon/www/youxindemo/customview/art/chapter1/FirstActivity.java
@@ -0,0 +1,31 @@
+package com.chinabluedon.www.youxindemo.customview.art.chapter1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
+
+import com.chinabluedon.www.youxindemo.R;
+
+/**
+ * @author 胡腾
+ * @time 2017/10/11  21:52
+ * @desc ${TODD}
+ */
+public class FirstActivity extends Activity {
+
+    private final static String TAG = "FirstActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+        Log.e(TAG, "taskid" + getTaskId());
+    }
+
+    public void click(View view) {
+        startActivity(new Intent(this,SecondActivity.class));
+    }
+}
